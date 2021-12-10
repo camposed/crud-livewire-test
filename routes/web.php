92d0c795028714803320f1f9c7ccc96fcc3aa40b
Edwin Alexis Campos Quele <ecampos@ipsfa.com>
2021-12-10 09:12:26 -0600

    Se agrego variable de entorno https 3

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,14 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Livewire\Scripts;
 
+if (env('APP_ENV') === 'production') {
+    URL::forceSchema('https');
+}
+
+
 Route::get('/', function () {
     return view('auth.login');
 });
-
 
 Route::middleware(['auth:sanctum', 'verified'])->group( function () {
     Route::get('/scripts', Scripts::class);
